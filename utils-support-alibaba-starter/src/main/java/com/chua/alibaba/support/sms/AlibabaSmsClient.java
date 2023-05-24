742b5adfa915cce4250575eacec7a1b46914349b
ACHTK <yemengzhidiandig@gmail.com>
2023-05-24 19:58:09 +0800

    no commit message

--- utils-support-alibaba-starter/src/main/java/com/chua/alibaba/support/sms/AlibabaSmsClient.java
+++ utils-support-alibaba-starter/src/main/java/com/chua/alibaba/support/sms/AlibabaSmsClient.java
@@ -1,165 +1,162 @@
-package com.chua.alibaba.support.sms;
-
-import com.aliyuncs.CommonRequest;
-import com.aliyuncs.CommonResponse;
-import com.aliyuncs.DefaultAcsClient;
-import com.aliyuncs.IAcsClient;
-import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.http.MethodType;
-import com.aliyuncs.profile.DefaultProfile;
-import com.aliyuncs.profile.IClientProfile;
-import com.chua.common.support.sms.AbstractSmsClient;
-import com.chua.common.support.sms.SmsException;
-import com.chua.common.support.sms.SmsTemplate;
-import com.google.gson.Gson;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Objects;
-
-import static com.chua.alibaba.support.sms.Utils.*;
-
-
-/**
- * 阿里云 SMS 客户端.
- *
- * @author cn-src
- */
-public class AlibabaSmsClient extends AbstractSmsClient<Object> {
-
-    private IAcsClient acsClient;
-    private final Gson gson = new Gson();
-
-
-    public AlibabaSmsClient(String accessKeyId, String accessKeySecret) {
-        super(accessKeyId, accessKeySecret, null);
-    }
-
-    public AlibabaSmsClient(String accessKeyId, String accessKeySecret, Map<String, SmsTemplate> smsTemplates) {
-        super(accessKeyId, accessKeySecret, smsTemplates, null);
-        final IClientProfile clientProfile = DefaultProfile.getProfile(
-                "default", accessKeyId, accessKeySecret);
-
-        this.acsClient = new DefaultAcsClient(clientProfile);
-    }
-
-    /**
-     * Instantiates a new SmsClient.
-     *
-     * @param acsClient    IAcsClient
-     * @param smsTemplates 预置短信模板
-     */
-    public AlibabaSmsClient(final IAcsClient acsClient, final Map<String, SmsTemplate> smsTemplates) {
-        super(null, null, smsTemplates, null);
-        this.acsClient = acsClient;
-    }
-
-    /**
-     * 发送短信验证码.
-     *
-     * @param phoneNumber 手机号码(中国)
-     *
-     * @return 6 位数的随机码
-     */
-    public int sendVerificationCode(final String smsTemplateKey, final String phoneNumber) {
-        checkPhoneNumber(phoneNumber);
-        final SmsTemplate smsTemplate = this.smsTemplates.get(smsTemplateKey);
-        Objects.requireNonNull(smsTemplate, () -> "SmsTemplate must be not null, key:" + smsTemplateKey);
-
-        final int code = Utils.randomCode();
-        smsTemplate.setTemplateParam(Collections.singletonMap("code", String.valueOf(code)));
-        smsTemplate.setPhoneNumbers(Collections.singletonList(phoneNumber));
-        send(smsTemplate);
-        return code;
-    }
-
-    /**
-     * 发送短信.
-     *
-     * @param smsTemplateKey 预置短信模板 key
-     */
-    public void send(final String smsTemplateKey) {
-        final SmsTemplate smsTemplate = this.smsTemplates.get(smsTemplateKey);
-        Objects.requireNonNull(smsTemplate, () -> "SmsTemplate must be not null, key:" + smsTemplateKey);
-
-        send(smsTemplate);
-    }
-
-    /**
-     * 发送短信.
-     *
-     * @param smsTemplateKey 预置短信模板 key
-     * @param phoneNumbers 手机号码，优先于预置短信模板中配置的手机号码
-     */
-    public void send(final String smsTemplateKey, final String... phoneNumbers) {
-        final SmsTemplate smsTemplate = this.smsTemplates.get(smsTemplateKey);
-        Objects.requireNonNull(smsTemplate, () -> "SmsTemplate must be not null, key:" + smsTemplateKey);
-
-        smsTemplate.setPhoneNumbers(Arrays.asList(phoneNumbers));
-        send(smsTemplate);
-    }
-
-    /**
-     * 发送短信.
-     *
-     * @param smsTemplate 短信模板
-     * @return
-     */
-    @Override
-    public Map<String, String> send(final SmsTemplate smsTemplate) {
-        Objects.requireNonNull(smsTemplate);
-        checkSmsTemplate(smsTemplate);
-
-        final CommonRequest request = new CommonRequest();
-        request.setSysMethod(MethodType.POST);
-        request.setSysDomain("dysmsapi.aliyuncs.com");
-        request.setSysVersion("2017-05-25");
-        request.setSysAction("SendSms");
-        request.putQueryParameter("PhoneNumbers", String.join(",", smsTemplate.getPhoneNumbers()));
-        request.putQueryParameter("SignName", smsTemplate.getSignName());
-        request.putQueryParameter("TemplateCode", smsTemplate.getTemplateCode());
-        request.putQueryParameter("TemplateParam", Utils.toJsonStr(smsTemplate.getTemplateParam()));
-        try {
-            final CommonResponse response = this.acsClient.getCommonResponse(request);
-            return checkSmsResponse(response);
-        }
-        catch (final ClientException e) {
-            throw new SmsException(e);
-        }
-    }
-
-    /**
-     * 批量发送短信.
-     *
-     * <p>
-     * 批量发送短信接口，支持在一次请求中分别向多个不同的手机号码发送不同签名的短信。
-     * 手机号码，签名，模板参数字段个数相同，一一对应，短信服务根据字段的顺序判断发往指定手机号码的签名。
-     *
-     * <p>
-     * 如果您需要往多个手机号码中发送同样签名的短信，请使用 {@link #send(SmsTemplate)}。
-     *
-     * @param batchSmsTemplate 批量发送短信模板
-     */
-    public void send(final BatchSmsTemplate batchSmsTemplate) {
-        Objects.requireNonNull(batchSmsTemplate);
-        checkBatchSmsTemplate(batchSmsTemplate);
-
-        final CommonRequest request = new CommonRequest();
-        request.setSysMethod(MethodType.POST);
-        request.setSysDomain("dysmsapi.aliyuncs.com");
-        request.setSysVersion("2017-05-25");
-        request.setSysAction("SendBatchSms");
-        request.putQueryParameter("PhoneNumberJson", this.gson.toJson(batchSmsTemplate.getPhoneNumbers()));
-        request.putQueryParameter("SignNameJson", this.gson.toJson(batchSmsTemplate.getSignNames()));
-        request.putQueryParameter("TemplateCode", batchSmsTemplate.getTemplateCode());
-        request.putQueryParameter("TemplateParamJson", this.gson.toJson(batchSmsTemplate.getTemplateParams()));
-        try {
-            final CommonResponse response = this.acsClient.getCommonResponse(request);
-            checkSmsResponse(response);
-        }
-        catch (final ClientException e) {
-            throw new SmsException(e);
-        }
-    }
-}
+//package com.chua.alibaba.support.sms;
+//
+//import com.aliyuncs.CommonRequest;
+//import com.aliyuncs.CommonResponse;
+//import com.aliyuncs.DefaultAcsClient;
+//import com.aliyuncs.IAcsClient;
+//import com.aliyuncs.exceptions.ClientException;
+//import com.aliyuncs.http.MethodType;
+//import com.aliyuncs.profile.DefaultProfile;
+//import com.aliyuncs.profile.IClientProfile;
+//import com.google.gson.Gson;
+//
+//import java.util.Arrays;
+//import java.util.Collections;
+//import java.util.Map;
+//import java.util.Objects;
+//
+//import static com.chua.alibaba.support.sms.Utils.*;
+//
+//
+///**
+// * 阿里云 SMS 客户端.
+// *
+// * @author cn-src
+// */
+//public class AlibabaSmsClient extends AbstractSmsClient<Object> {
+//
+//    private IAcsClient acsClient;
+//    private final Gson gson = new Gson();
+//
+//
+//    public AlibabaSmsClient(String accessKeyId, String accessKeySecret) {
+//        super(accessKeyId, accessKeySecret, null);
+//    }
+//
+//    public AlibabaSmsClient(String accessKeyId, String accessKeySecret, Map<String, SmsTemplate> smsTemplates) {
+//        super(accessKeyId, accessKeySecret, smsTemplates, null);
+//        final IClientProfile clientProfile = DefaultProfile.getProfile(
+//                "default", accessKeyId, accessKeySecret);
+//
+//        this.acsClient = new DefaultAcsClient(clientProfile);
+//    }
+//
+//    /**
+//     * Instantiates a new SmsClient.
+//     *
+//     * @param acsClient    IAcsClient
+//     * @param smsTemplates 预置短信模板
+//     */
+//    public AlibabaSmsClient(final IAcsClient acsClient, final Map<String, SmsTemplate> smsTemplates) {
+//        super(null, null, smsTemplates, null);
+//        this.acsClient = acsClient;
+//    }
+//
+//    /**
+//     * 发送短信验证码.
+//     *
+//     * @param phoneNumber 手机号码(中国)
+//     *
+//     * @return 6 位数的随机码
+//     */
+//    public int sendVerificationCode(final String smsTemplateKey, final String phoneNumber) {
+//        checkPhoneNumber(phoneNumber);
+//        final SmsTemplate smsTemplate = this.smsTemplates.get(smsTemplateKey);
+//        Objects.requireNonNull(smsTemplate, () -> "SmsTemplate must be not null, key:" + smsTemplateKey);
+//
+//        final int code = Utils.randomCode();
+//        smsTemplate.setTemplateParam(Collections.singletonMap("code", String.valueOf(code)));
+//        smsTemplate.setPhoneNumbers(Collections.singletonList(phoneNumber));
+//        send(smsTemplate);
+//        return code;
+//    }
+//
+//    /**
+//     * 发送短信.
+//     *
+//     * @param smsTemplateKey 预置短信模板 key
+//     */
+//    public void send(final String smsTemplateKey) {
+//        final SmsTemplate smsTemplate = this.smsTemplates.get(smsTemplateKey);
+//        Objects.requireNonNull(smsTemplate, () -> "SmsTemplate must be not null, key:" + smsTemplateKey);
+//
+//        send(smsTemplate);
+//    }
+//
+//    /**
+//     * 发送短信.
+//     *
+//     * @param smsTemplateKey 预置短信模板 key
+//     * @param phoneNumbers 手机号码，优先于预置短信模板中配置的手机号码
+//     */
+//    public void send(final String smsTemplateKey, final String... phoneNumbers) {
+//        final SmsTemplate smsTemplate = this.smsTemplates.get(smsTemplateKey);
+//        Objects.requireNonNull(smsTemplate, () -> "SmsTemplate must be not null, key:" + smsTemplateKey);
+//
+//        smsTemplate.setPhoneNumbers(Arrays.asList(phoneNumbers));
+//        send(smsTemplate);
+//    }
+//
+//    /**
+//     * 发送短信.
+//     *
+//     * @param smsTemplate 短信模板
+//     * @return
+//     */
+//    @Override
+//    public Map<String, String> send(final SmsTemplate smsTemplate) {
+//        Objects.requireNonNull(smsTemplate);
+//        checkSmsTemplate(smsTemplate);
+//
+//        final CommonRequest request = new CommonRequest();
+//        request.setSysMethod(MethodType.POST);
+//        request.setSysDomain("dysmsapi.aliyuncs.com");
+//        request.setSysVersion("2017-05-25");
+//        request.setSysAction("SendSms");
+//        request.putQueryParameter("PhoneNumbers", String.join(",", smsTemplate.getPhoneNumbers()));
+//        request.putQueryParameter("SignName", smsTemplate.getSignName());
+//        request.putQueryParameter("TemplateCode", smsTemplate.getTemplateCode());
+//        request.putQueryParameter("TemplateParam", Utils.toJsonStr(smsTemplate.getTemplateParam()));
+//        try {
+//            final CommonResponse response = this.acsClient.getCommonResponse(request);
+//            return checkSmsResponse(response);
+//        }
+//        catch (final ClientException e) {
+//            throw new SmsException(e);
+//        }
+//    }
+//
+//    /**
+//     * 批量发送短信.
+//     *
+//     * <p>
+//     * 批量发送短信接口，支持在一次请求中分别向多个不同的手机号码发送不同签名的短信。
+//     * 手机号码，签名，模板参数字段个数相同，一一对应，短信服务根据字段的顺序判断发往指定手机号码的签名。
+//     *
+//     * <p>
+//     * 如果您需要往多个手机号码中发送同样签名的短信，请使用 {@link #send(SmsTemplate)}。
+//     *
+//     * @param batchSmsTemplate 批量发送短信模板
+//     */
+//    public void send(final BatchSmsTemplate batchSmsTemplate) {
+//        Objects.requireNonNull(batchSmsTemplate);
+//        checkBatchSmsTemplate(batchSmsTemplate);
+//
+//        final CommonRequest request = new CommonRequest();
+//        request.setSysMethod(MethodType.POST);
+//        request.setSysDomain("dysmsapi.aliyuncs.com");
+//        request.setSysVersion("2017-05-25");
+//        request.setSysAction("SendBatchSms");
+//        request.putQueryParameter("PhoneNumberJson", this.gson.toJson(batchSmsTemplate.getPhoneNumbers()));
+//        request.putQueryParameter("SignNameJson", this.gson.toJson(batchSmsTemplate.getSignNames()));
+//        request.putQueryParameter("TemplateCode", batchSmsTemplate.getTemplateCode());
+//        request.putQueryParameter("TemplateParamJson", this.gson.toJson(batchSmsTemplate.getTemplateParams()));
+//        try {
+//            final CommonResponse response = this.acsClient.getCommonResponse(request);
+//            checkSmsResponse(response);
+//        }
+//        catch (final ClientException e) {
+//            throw new SmsException(e);
+//        }
+//    }
+//}
--- utils-support-alibaba-starter/src/main/java/com/chua/alibaba/support/sms/Utils.java
+++ utils-support-alibaba-starter/src/main/java/com/chua/alibaba/support/sms/Utils.java
@@ -1,146 +1,146 @@
-package com.chua.alibaba.support.sms;
-
-import com.aliyuncs.CommonResponse;
-import com.chua.common.support.sms.SmsException;
-import com.chua.common.support.sms.SmsTemplate;
-import com.google.gson.Gson;
-
-import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ThreadLocalRandom;
-
-/**
- * 工具类，内部使用.
- *
- * @author cn-src
- */
-class Utils {
-
-    private static final String SUCCESS_CODE = "OK";
-    /**
-     * 宽松校验即可.
-     */
-    private static final String PHONE_NUMBER_REGEX = "\\d{5,}";
-
-    /**
-     * 生成随机验证码.
-     *
-     * @return 随机数
-     */
-    static int randomCode() {
-        return 100_000 + ThreadLocalRandom.current().nextInt(1_000_000 - 100_000);
-    }
-
-    /**
-     * Map 转 json 字符串的简单实现.
-     *
-     * @param map the map
-     *
-     * @return the json string
-     */
-    static String toJsonStr(final Map<String, String> map) {
-        if (null == map || map.isEmpty()) {
-            return null;
-        }
-
-        final StringBuilder sb = new StringBuilder();
-        sb.append('{');
-        for (final Map.Entry<String, String> entry : map.entrySet()) {
-            sb.append('"')
-                    .append(entry.getKey().replace("\"", "\\\""))
-                    .append('"')
-                    .append(':')
-                    .append('"')
-                    .append(entry.getValue().replace("\"", "\\\""))
-                    .append('"')
-                    .append(',');
-        }
-        sb.deleteCharAt(sb.length() - 1);
-        sb.append('}');
-        return sb.toString();
-    }
-
-    /**
-     * 校验 SmsTemplate.
-     *
-     * @param template the SmsTemplate
-     */
-    static void checkSmsTemplate(final SmsTemplate template) {
-
-        checkNotEmpty(template.getSignName(), "SmsTemplate signName must be not empty");
-        checkNotEmpty(template.getTemplateCode(), "SmsTemplate templateCode must be not empty");
-        checkNotEmpty(template.getPhoneNumbers(), "SmsTemplate phoneNumbers must be not empty");
-
-    }
-
-    /**
-     * 校验 BatchSmsTemplate.
-     *
-     * @param template the BatchSmsTemplate
-     */
-    static void checkBatchSmsTemplate(final BatchSmsTemplate template) {
-
-        checkNotEmpty(template.getSignNames(), "BatchSmsTemplate signNames must be not empty");
-        checkNotEmpty(template.getPhoneNumbers(), "BatchSmsTemplate phoneNumbers must be not empty");
-        checkNotEmpty(template.getTemplateCode(), "BatchSmsTemplate templateCode must be not empty");
-        checkNotEmpty(template.getTemplateParams(), "BatchSmsTemplate templateParams must be not empty");
-
-        if (template.getSignNames().size() != template.getPhoneNumbers().size()
-                && template.getPhoneNumbers().size() != template.getTemplateParams().size()) {
-            throw new IllegalArgumentException("BatchSmsTemplate phoneNumbers, signNames, templateParams size must be the same");
-        }
-    }
-
-    /**
-     * 校验 SendSmsResponse 状态.
-     *
-     * @param response the SendSmsResponse
-     * @return res
-     */
-    static Map<String, String> checkSmsResponse(final CommonResponse response) {
-        if (null == response) {
-            throw new SmsException("Response is null");
-        }
-        final Gson gson = new Gson();
-        final Map<String, String> json = gson.fromJson(response.getData(), Map.class);
-        if (!SUCCESS_CODE.equalsIgnoreCase(json.get("Code"))) {
-            throw new SmsException("Http status: " + response.getHttpStatus() + ", response: " + response.getData());
-        }
-        return json;
-    }
-
-    /**
-     * 校验手机号码（中国）.
-     *
-     * @param phoneNumber the phone number
-     */
-    static void checkPhoneNumber(final String phoneNumber) {
-        if (null == phoneNumber || !phoneNumber.matches(PHONE_NUMBER_REGEX)) {
-            throw new IllegalArgumentException("Invalid phone number");
-        }
-    }
-
-    /**
-     * 校验字符串不为空.
-     *
-     * @param str the str
-     * @param message the message
-     */
-    static void checkNotEmpty(final String str, final String message) {
-        if (null == str || str.isEmpty()) {
-            throw new IllegalArgumentException(message);
-        }
-    }
-
-    /**
-     * 校验集合不为空.
-     *
-     * @param coll the Collection
-     * @param message the message
-     */
-    static void checkNotEmpty(final Collection coll, final String message) {
-        if (null == coll || coll.isEmpty()) {
-            throw new IllegalArgumentException(message);
-        }
-    }
-}
+//package com.chua.alibaba.support.sms;
+//
+//import com.aliyuncs.CommonResponse;
+//import com.chua.common.support.sms.SmsException;
+//import com.chua.common.support.sms.SmsTemplate;
+//import com.google.gson.Gson;
+//
+//import java.util.Collection;
+//import java.util.Map;
+//import java.util.concurrent.ThreadLocalRandom;
+//
+///**
+// * 工具类，内部使用.
+// *
+// * @author cn-src
+// */
+//class Utils {
+//
+//    private static final String SUCCESS_CODE = "OK";
+//    /**
+//     * 宽松校验即可.
+//     */
+//    private static final String PHONE_NUMBER_REGEX = "\\d{5,}";
+//
+//    /**
+//     * 生成随机验证码.
+//     *
+//     * @return 随机数
+//     */
+//    static int randomCode() {
+//        return 100_000 + ThreadLocalRandom.current().nextInt(1_000_000 - 100_000);
+//    }
+//
+//    /**
+//     * Map 转 json 字符串的简单实现.
+//     *
+//     * @param map the map
+//     *
+//     * @return the json string
+//     */
+//    static String toJsonStr(final Map<String, String> map) {
+//        if (null == map || map.isEmpty()) {
+//            return null;
+//        }
+//
+//        final StringBuilder sb = new StringBuilder();
+//        sb.append('{');
+//        for (final Map.Entry<String, String> entry : map.entrySet()) {
+//            sb.append('"')
+//                    .append(entry.getKey().replace("\"", "\\\""))
+//                    .append('"')
+//                    .append(':')
+//                    .append('"')
+//                    .append(entry.getValue().replace("\"", "\\\""))
+//                    .append('"')
+//                    .append(',');
+//        }
+//        sb.deleteCharAt(sb.length() - 1);
+//        sb.append('}');
+//        return sb.toString();
+//    }
+//
+//    /**
+//     * 校验 SmsTemplate.
+//     *
+//     * @param template the SmsTemplate
+//     */
+//    static void checkSmsTemplate(final SmsTemplate template) {
+//
+//        checkNotEmpty(template.getSignName(), "SmsTemplate signName must be not empty");
+//        checkNotEmpty(template.getTemplateCode(), "SmsTemplate templateCode must be not empty");
+//        checkNotEmpty(template.getPhoneNumbers(), "SmsTemplate phoneNumbers must be not empty");
+//
+//    }
+//
+//    /**
+//     * 校验 BatchSmsTemplate.
+//     *
+//     * @param template the BatchSmsTemplate
+//     */
+//    static void checkBatchSmsTemplate(final BatchSmsTemplate template) {
+//
+//        checkNotEmpty(template.getSignNames(), "BatchSmsTemplate signNames must be not empty");
+//        checkNotEmpty(template.getPhoneNumbers(), "BatchSmsTemplate phoneNumbers must be not empty");
+//        checkNotEmpty(template.getTemplateCode(), "BatchSmsTemplate templateCode must be not empty");
+//        checkNotEmpty(template.getTemplateParams(), "BatchSmsTemplate templateParams must be not empty");
+//
+//        if (template.getSignNames().size() != template.getPhoneNumbers().size()
+//                && template.getPhoneNumbers().size() != template.getTemplateParams().size()) {
+//            throw new IllegalArgumentException("BatchSmsTemplate phoneNumbers, signNames, templateParams size must be the same");
+//        }
+//    }
+//
+//    /**
+//     * 校验 SendSmsResponse 状态.
+//     *
+//     * @param response the SendSmsResponse
+//     * @return res
+//     */
+//    static Map<String, String> checkSmsResponse(final CommonResponse response) {
+//        if (null == response) {
+//            throw new SmsException("Response is null");
+//        }
+//        final Gson gson = new Gson();
+//        final Map<String, String> json = gson.fromJson(response.getData(), Map.class);
+//        if (!SUCCESS_CODE.equalsIgnoreCase(json.get("Code"))) {
+//            throw new SmsException("Http status: " + response.getHttpStatus() + ", response: " + response.getData());
+//        }
+//        return json;
+//    }
+//
+//    /**
+//     * 校验手机号码（中国）.
+//     *
+//     * @param phoneNumber the phone number
+//     */
+//    static void checkPhoneNumber(final String phoneNumber) {
+//        if (null == phoneNumber || !phoneNumber.matches(PHONE_NUMBER_REGEX)) {
+//            throw new IllegalArgumentException("Invalid phone number");
+//        }
+//    }
+//
+//    /**
+//     * 校验字符串不为空.
+//     *
+//     * @param str the str
+//     * @param message the message
+//     */
+//    static void checkNotEmpty(final String str, final String message) {
+//        if (null == str || str.isEmpty()) {
+//            throw new IllegalArgumentException(message);
+//        }
+//    }
+//
+//    /**
+//     * 校验集合不为空.
+//     *
+//     * @param coll the Collection
+//     * @param message the message
+//     */
+//    static void checkNotEmpty(final Collection coll, final String message) {
+//        if (null == coll || coll.isEmpty()) {
+//            throw new IllegalArgumentException(message);
+//        }
+//    }
+//}
