6898011a905d3c928d021622ee43597544dab9be
ACHTK <yemengzhidiandig@gmail.com>
2023-06-21 17:19:08 +0800

    调整代码

---
+++ utils-support-common-starter/src/main/java/com/chua/common/support/lang/arrange/Arrange.java
@@ -0,0 +1,10 @@
+package com.chua.common.support.lang.arrange;
+
+/**
+ * 编排
+ * @author CH
+ */
+public class Arrange {
+
+
+}
--- utils-support-example-starter/src/main/java/com/chua/example/module/Module2Example.java
+++ utils-support-example-starter/src/main/java/com/chua/example/module/Module2Example.java
@@ -1,9 +1,8 @@
-package com.chua.example.module
+package com.chua.example.module;
 
 import com.alibaba.fastjson2.JSONObject;
 import com.chua.common.support.collection.ImmutableBuilder;
-import com.chua.common.support.json.JsonObject;
-import com.chua.common.support.modularity.Modularity;
+import com.chua.common.support.modularity.Modularity;;
 import com.chua.common.support.modularity.ModularityFactory;
 import com.chua.common.support.modularity.ModularityResult;
 import com.chua.common.support.modularity.MsgEvent;
@@ -11,32 +10,31 @@ import com.chua.common.support.utils.ThreadUtils;
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.YieldingWaitStrategy;
 import com.lmax.disruptor.dsl.Disruptor;
-import com.lmax.disruptor.dsl.ProducerType
-import com.mysql.cj.jdbc.Driver;
+import com.lmax.disruptor.dsl.ProducerType;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.Executor;
 
 @Slf4j
-class ModuleExample {
+class Module2Example {
 
     static void main2(String[] args) {
 
     }
 
-    static void main(String[] args) {
-        ModularityFactory modularityFactory = ModularityFactory.create()
+    public static void main(String[] args) {
+        ModularityFactory modularityFactory = ModularityFactory.create();
         modularityFactory.register(Modularity.builder()
-                .moduleType('http').moduleName("hkToken").moduleDesc("获取海康token")
+                .moduleType("http").moduleName("hkToken").moduleDesc("获取海康token")
                 .moduleScript("POST https://open.hikyun.com/artemis/oauth/token/v2")
-                .moduleRequest("""secretKey:#{secretKey};accessKey:#{accessKey};productCode: #{1664360485020781}""")
-                .build())
+                .moduleRequest("secretKey:#{secretKey};accessKey:#{accessKey};productCode: #{1664360485020781}")
+                .build());
 
         modularityFactory.register(Modularity.builder()
                 .moduleType("http").moduleName("hk_page").moduleDesc("分页查询海康设备数据")
                 .moduleScript("POST https://open.hikyun.com/artemis/api/eits/v2/global/device/page")
-                .moduleHeader("""access_token: #{hkToken.data.access_token}""")
-                .moduleRequest("""[{"key": "projectId", "option": "eq", "value": 651515588253424 }]""")
+                .moduleHeader("access_token: #{hkToken.data.access_token}")
+                .moduleRequest("[{key: \"projectId\", option: \"eq\", value: 651515588253424 }]")
                 .moduleDepends("http:hkToken").build());
         ModularityResult modularityResult = modularityFactory.execute("http", "hk_page", ImmutableBuilder.builderOfStringMap().build());
 
@@ -55,7 +53,7 @@ class ModuleExample {
         ExampleEventHandler handler32 = ExampleEventHandler.of("32");
 
         disruptor.handleEventsWith(handler11);
-        disruptor.after(handler11).handleEventsWith(handler12)
+        disruptor.after(handler11).handleEventsWith(handler12);
 
         disruptor.start();
 
