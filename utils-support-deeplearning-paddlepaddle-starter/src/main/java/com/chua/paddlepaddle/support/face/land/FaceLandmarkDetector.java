db67cdad34b53fd432d12680dc0ea01ee450369f
ACHTK <yemengzhidiandig@gmail.com>
2023-08-09 22:06:53 +0800

    no commit message

---
+++ utils-support-deeplearning-paddlepaddle-starter/src/main/java/com/chua/paddlepaddle/support/face/land/FaceLandmarkDetector.java
@@ -0,0 +1,116 @@
+package com.chua.paddlepaddle.support.face.land;
+
+import ai.djl.inference.Predictor;
+import ai.djl.modality.cv.Image;
+import ai.djl.translate.TranslateException;
+import com.chua.common.support.bean.BeanUtils;
+import com.chua.common.support.constant.PredictResult;
+import com.chua.common.support.feature.DetectionConfiguration;
+import com.chua.common.support.feature.detector.Detector;
+import com.chua.common.support.json.Json;
+import com.chua.common.support.utils.CollectionUtils;
+import com.chua.common.support.utils.StringUtils;
+import com.chua.pytorch.support.AbstractPytorchDetector;
+import com.chua.pytorch.support.utils.LocationUtils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.RenderedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 关键点
+ *
+ * @author CH
+ */
+public class FaceLandmarkDetector extends AbstractPytorchDetector<float[][]> {
+
+    private final Detector detector;
+
+    public FaceLandmarkDetector(DetectionConfiguration configuration, Detector detector) {
+        super(configuration,
+                new FaceLandmarkTranslator(),
+                "PaddlePaddle",
+                "inference",
+                StringUtils.defaultString(configuration.modelPath(), "face_landmark"),
+                "https://aias-home.oss-cn-beijing.aliyuncs.com/models/face_landmark.zip",
+                true);
+        this.detector = detector;
+    }
+
+    @Override
+    protected Class<float[][]> type() {
+        return float[][].class;
+    }
+
+    @Override
+    public List<PredictResult> detect(Object face) {
+        List<PredictResult> detect = detector.detect(face);
+        if (CollectionUtils.isEmpty(detect)) {
+            return Collections.emptyList();
+        }
+
+        List<PredictResult> results = new LinkedList<>();
+        Image img = LocationUtils.getImage(face);
+        for (PredictResult predictResult : detect) {
+
+            try (Predictor<Image, float[][]> predictor = model.newPredictor()) {
+                Image subImage = LocationUtils.getSubImage(img, LocationUtils.getBoundingBox(predictResult), 0f);
+                float[][] o = predictor.predict(subImage);
+                PredictResult item = new PredictResult();
+                BeanUtils.copyProperties(predictResult, item);
+                item.setNdArray(Json.toJson(o));
+
+                results.add(item);
+            } catch (TranslateException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return results;
+    }
+
+    @Override
+    public void detect(Object face, OutputStream outputStream) throws Exception {
+        List<PredictResult> detect = detector.detect(face);
+        if (CollectionUtils.isEmpty(detect)) {
+            return;
+        }
+
+        Image img = LocationUtils.getImage(face);
+        Image image = img.duplicate();
+        for (PredictResult predictResult : detect) {
+
+            try (Predictor<Image, float[][]> predictor = model.newPredictor()) {
+                Image subImage = LocationUtils.getSubImage(img, LocationUtils.getBoundingBox(predictResult), 0f);
+                float[][] o = predictor.predict(subImage);
+                LocationUtils.drawLandmark(image, LocationUtils.getBoundingBox(predictResult), o[0]);
+
+            } catch (TranslateException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        try {
+            ImageIO.write((RenderedImage) image.getWrappedImage(), "png", outputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    protected List<PredictResult> toDetect(float[][] o, Image img) {
+        return null;
+    }
+
+
+    @Override
+    public void close() throws Exception {
+        super.close();
+        detector.close();
+    }
+}
---
+++ utils-support-deeplearning-paddlepaddle-starter/src/main/java/com/chua/paddlepaddle/support/face/recognizer/PaddleFeatureRecognizer.java
@@ -0,0 +1,87 @@
+package com.chua.paddlepaddle.support.face.recognizer;
+
+import ai.djl.inference.Predictor;
+import ai.djl.modality.cv.Image;
+import ai.djl.translate.TranslateException;
+import com.chua.common.support.constant.PredictResult;
+import com.chua.common.support.feature.DetectionConfiguration;
+import com.chua.common.support.feature.detector.Detector;
+import com.chua.pytorch.support.AbstractPytorchRecognizer;
+import com.chua.pytorch.support.face.net.FaceLabelNet;
+import com.chua.pytorch.support.utils.LocationUtils;
+
+/**
+ * PaddlePaddle
+ *
+ * @author CH
+ */
+public class PaddleFeatureRecognizer extends AbstractPytorchRecognizer<float[]> {
+    private final FaceLabelNet faceLabelNet;
+
+    public PaddleFeatureRecognizer(Detector detector, DetectionConfiguration configuration, FaceLabelNet faceLabelNet) {
+        super(detector, configuration,
+                new PaddlePaddleFaceFeatureTranslator(),
+                "PaddlePaddle",
+                "inference",
+                "arcface_iresnet50_v1.0_infer",
+                "https://aias-home.oss-cn-beijing.aliyuncs.com/models/sec_models/MobileFace.zip",
+                true);
+
+        this.faceLabelNet = faceLabelNet;
+    }
+
+    @Override
+    public float[] predict(Object img) {
+        Image image = LocationUtils.getImage(img);
+
+        if (null == image) {
+            return null;
+        }
+
+        try (Predictor<Image, float[]> predictor = model.newPredictor()) {
+            try {
+                return predictor.predict(image);
+            } catch (TranslateException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected PredictResult createPredictResult(Image subImage) {
+        float[] feature = new float[0];
+        try (Predictor<Image, float[]> predictor = model.newPredictor()) {
+            try {
+                feature = predictor.predict(subImage);
+            } catch (TranslateException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (null == faceLabelNet) {
+            return null;
+
+        }
+        PredictResult result = new PredictResult();
+        //推理是属于哪个lab
+        FaceLabelNet.MaxLab maxLab = null;
+        for (int i = 0; i < faceLabelNet.features(); i++) {
+            maxLab = faceLabelNet.predict(feature);
+            if (null == maxLab) {
+                continue;
+            }
+
+            result.setClsLabel(maxLab.maxLab);
+            result.setClsScore(maxLab.maxSimilar);
+            result.setScore(maxLab.maxSimilar);
+            result.setText(maxLab.maxLab);
+        }
+        return result;
+    }
+
+    @Override
+    protected Class<float[]> type() {
+        return float[].class;
+    }
+}
