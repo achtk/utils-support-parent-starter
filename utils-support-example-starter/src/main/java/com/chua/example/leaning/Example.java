50b4e8ead06f3394ed95d85d3a63d615f0ed8f61
ACHTK <yemengzhidiandig@gmail.com>
2023-08-17 14:58:58 +0800

    1、添加统一中心订阅脚本

--- utils-support-example-starter/src/main/java/com/chua/example/leaning/Example.java
+++ utils-support-example-starter/src/main/java/com/chua/example/leaning/Example.java
@@ -1,6 +1,7 @@
 package com.chua.example.leaning;
 
-import org.zbus.mq.server.MqServer;
+import com.chua.common.support.utils.ByteUtils;
+import com.chua.common.support.utils.Hex;
 
 /**
  * @author CH
@@ -8,8 +9,18 @@ import org.zbus.mq.server.MqServer;
 public class Example {
 
     public static void main(String[] args) throws Exception {
-        MqServer mqServer = new MqServer();
-        mqServer.start();
+        String hex = "55 A1 01 E4 03 01 32 00";
+        String validate;
+        int tpl = 0;
+        for (String s : hex.split("\\s+")) {
+            int i = Integer.valueOf(s, 16);
+            tpl ^= i;
+        }
+
+        System.out.println(Hex.everySpace(Hex.encodeHexUpperString(ByteUtils.asBytes(tpl))));
+
+//        MqServer mqServer = new MqServer();
+//        mqServer.start();
 //        Integer[] arr = {832,833,834,1040,1041,1042,1095,1096,1097,1098,1099,1100,1101,1102,1229,1230,1231,1232,1233,1234,1235};
 //
 //        Arrays.sort(arr);
