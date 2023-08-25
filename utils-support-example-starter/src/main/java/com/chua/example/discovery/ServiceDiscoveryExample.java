0abea1dbf92224bb162a61fb2cb52b1fbb9fbc74
ACHTK <yemengzhidiandig@gmail.com>
2023-08-25 11:29:12 +0800

    1、微信接口

--- utils-support-example-starter/src/main/java/com/chua/example/discovery/ServiceDiscoveryExample.java
+++ utils-support-example-starter/src/main/java/com/chua/example/discovery/ServiceDiscoveryExample.java
@@ -18,9 +18,9 @@ public class ServiceDiscoveryExample {
     public static void main(String[] args) throws Exception {
         ServiceDiscovery serviceDiscovery = ServiceProvider.of(ServiceDiscovery.class).getExtension("multicast");
         serviceDiscovery.start(new DiscoveryOption().setAddress("224.2.2.4:6379"));
-        serviceDiscovery.register(Discovery.builder().discovery("/danbin").address("http://qrtest.qirenit.com:81").build());
-        serviceDiscovery.register(Discovery.builder().discovery("/danbin").address("http://qrtest.qirenit.com:82").build());
-        serviceDiscovery.register(Discovery.builder().discovery("/webrtc").address("https://webrtc.tzbyte.com:6443").build());
+        serviceDiscovery.register(Discovery.builder().discovery("/danbin").address("").build());
+        serviceDiscovery.register(Discovery.builder().discovery("/danbin").address("").build());
+        serviceDiscovery.register(Discovery.builder().discovery("/webrtc").address("").build());
 
         System.out.println();
         while (true) {
--- utils-support-example-starter/src/main/java/com/chua/example/view/ViewFactoryExample.java
+++ utils-support-example-starter/src/main/java/com/chua/example/view/ViewFactoryExample.java
@@ -18,11 +18,11 @@ public class ViewFactoryExample {
     public static void main(String[] args) throws Exception {
         ViewServer viewServer = new HttpViewServer();
         viewServer.addPlugin("water", new TextWaterImageFilter("我是水印", Color.BLACK));
-        viewServer.addPlugin("logo", new ImageWaterImageFilter(new URL("https://cdn.qirenit.com/8d11da41-4f8d-4bcc-a36e-d36cddea2021").openStream()));
-        viewServer.addPlugin("logo-text", new TextImgWaterImageFilter("我是水印", new URL("https://cdn.qirenit.com/8d11da41-4f8d-4bcc-a36e-d36cddea2021").openStream(), new com.chua.common.support.protocol.image.ImagePoint(40, 20)));
+        viewServer.addPlugin("logo", new ImageWaterImageFilter(new URL("").openStream()));
+        viewServer.addPlugin("logo-text", new TextImgWaterImageFilter("我是水印", new URL("").openStream(), new com.chua.common.support.protocol.image.ImagePoint(40, 20)));
         viewServer.addContext("demo", ViewConfig.newBuilder().setPath("D://"));
         viewServer.addContext("demo1", ViewConfig.newBuilder().setType("minio").setPath("http://127.0.0.1:9000").setAppKey("minioadmin").setAppSecret("minioadmin"));
-        viewServer.addContext("achtk", ViewConfig.newBuilder().setType("alibaba").setAppKey("").setAppSecret("NElUp68vSAosQRCAKAOZNgGCiPeswQ"));
+        viewServer.addContext("achtk", ViewConfig.newBuilder().setType("alibaba").setAppKey("").setAppSecret(""));
         viewServer.run(new String[]{"97"});
     }
 }
