d2e9ef8380f3f436d57e115a1937db0989ab8bcc
ACHTK <yemengzhidiandig@gmail.com>
2023-08-25 11:32:23 +0800

    1、微信接口

--- utils-support-example-starter/src/main/java/com/chua/example/tokenizer/TokenizerExample.java
+++ utils-support-example-starter/src/main/java/com/chua/example/tokenizer/TokenizerExample.java
@@ -24,18 +24,16 @@ public class TokenizerExample {
 
     public static void main1(String[] args) {
         Map<String, Object> param = ImmutableBuilder.<String, Object>builderOfMap()
-//                .put("appid", "wx980065354062cb26")
-//                .put("mch_id", "1625485411")
                 .put("device_info", "013467007045764")
-                .put("nonce_str", "5K8264ILTKCH16CQ2502SI8ZNMTM67VS")
+                .put("nonce_str", "")
                 .put("body", "image形象店-深圳腾大- QQ公仔")
-                .put("out_trade_no", "130390382306112559")
+                .put("out_trade_no", "")
                 .put("total_fee", 1)
                 .put("spbill_create_ip", "8.8.8.8")
-                .put("auth_code", "130390382306112559")
+                .put("auth_code", "")
                 .build();
         Map<String, Object> newParam = MapUtils.sortKey(param);
-        String join = Joiner.on("&").withKeyValueSeparator("=").join(newParam) + "&key=C633B948583A4D76A5745F466E3950CD";
+        String join = Joiner.on("&").withKeyValueSeparator("=").join(newParam) + "&key=";
         System.out.println(join);
         String sign = Md5Utils.getInstance().getMd5String(join).toUpperCase();
         System.out.println(sign);
