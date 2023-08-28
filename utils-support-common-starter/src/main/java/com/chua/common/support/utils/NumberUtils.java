adc05e14cb8c9ff0ea006ed5089a0bdbdbca5c61
ACHTK <yemengzhidiandig@gmail.com>
2023-08-28 10:10:21 +0800

    1、代码规范调整

--- utils-support-common-starter/src/main/java/com/chua/common/support/utils/NumberUtils.java
+++ utils-support-common-starter/src/main/java/com/chua/common/support/utils/NumberUtils.java
@@ -2450,7 +2450,7 @@ public class NumberUtils {
      * @return 结果
      */
     public static double isZero(double value, double defaultValue) {
-        return Double.compare(0.0d, value) == 1  ? defaultValue : value;
+        return Double.compare(0.0d, value) == 0  ? defaultValue : value;
     }
 
     /**
