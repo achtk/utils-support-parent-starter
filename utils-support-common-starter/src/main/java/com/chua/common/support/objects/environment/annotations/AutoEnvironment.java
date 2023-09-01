f5bc304ad3b25124a2c3490e0c41a71b314ca9e8
ACHTK <yemengzhidiandig@gmail.com>
2023-09-01 17:33:43 +0800

    fix 1、添加对象管理器

---
+++ utils-support-common-starter/src/main/java/com/chua/common/support/objects/environment/annotations/AutoEnvironment.java
@@ -0,0 +1,12 @@
+package com.chua.common.support.objects.environment.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * 自动环境变量
+ * @author CH
+ */
+@Target(ElementType.TYPE)
+public @interface AutoEnvironment {
+}
