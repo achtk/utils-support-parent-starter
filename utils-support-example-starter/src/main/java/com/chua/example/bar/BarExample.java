0c05425b40b7a974dd4801b5a89c5ce4bc70f6f0
ACHTK <yemengzhidiandig@gmail.com>
2023-09-06 08:36:37 +0800

    fix 1、重命名部分文件

--- utils-support-example-starter/src/main/java/com/chua/example/bar/BarExample.java
+++ utils-support-example-starter/src/main/java/com/chua/example/bar/BarExample.java
@@ -1,15 +1,21 @@
 package com.chua.example.bar;
 
-import com.chua.common.support.lang.bar.BarCodeBuilder;
+import com.chua.common.support.lang.qr.QrCode;
+import com.chua.common.support.lang.qr.QrCodeConfigure;
+import com.chua.common.support.spi.ServiceProvider;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * @author CH
  */
 public class BarExample {
 
-    public static void main(String[] args) {
-        BarCodeBuilder
-                .newBuilder()
-                .codeEyesFormat(BarCodeBuilder.QrCodeEyesFormat.C_BORDER_C_POINT)
+    public static void main(String[] args) throws IOException {
+        QrCode qrCode = ServiceProvider.of(QrCode.class).getNewExtension("zxing", QrCodeConfigure.builder().content("233").build());
+        qrCode.out(Files.newOutputStream(Paths.get("Z://code.png")));
+        System.out.println();
     }
 }
