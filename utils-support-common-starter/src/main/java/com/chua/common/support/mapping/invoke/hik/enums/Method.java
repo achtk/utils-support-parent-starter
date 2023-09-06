c8c704a6f47cea0253e3c47542340d5d67af1ed4
ACHTK <yemengzhidiandig@gmail.com>
2023-09-06 21:06:22 +0800

    fix 1.代碼規範

--- utils-support-common-starter/src/main/java/com/chua/common/support/mapping/invoke/hik/enums/Method.java
+++
@@ -1,9 +0,0 @@
-package com.chua.common.support.mapping.invoke.hik.enums;
-
-/**
- * @author zhangtuo
- * @Date 2017/4/26
- */
-public enum Method {
-    GET, POST_FORM, POST_STRING, POST_BYTES, PUT_FORM, PUT_STRING, PUT_BYTES, DELETE, GET_RESPONSE, POST_FORM_RESPONSE, POST_STRING_RESPONSE;
-}
