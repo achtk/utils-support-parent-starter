7db2df71516c9bf56a6a6b8ccfe0cbb70502890c
ACHTK <yemengzhidiandig@gmail.com>
2023-09-07 17:27:10 +0800

    fix 1、重构部分对象

--- utils-support-common-starter/src/main/java/com/chua/common/support/lang/proxy/plugin/processor/CacheMethodAnnotationPostProcessor.java
+++ utils-support-common-starter/src/main/java/com/chua/common/support/lang/proxy/plugin/processor/CacheMethodAnnotationPostProcessor.java
@@ -6,6 +6,7 @@ import com.chua.common.support.objects.definition.element.MethodDescribe;
 import com.chua.common.support.spi.ServiceProvider;
 import com.chua.common.support.task.cache.Cache;
 import com.chua.common.support.task.cache.CacheConfiguration;
+import com.chua.common.support.task.cache.CacheEvict;
 import com.chua.common.support.task.cache.Cacheable;
 import com.chua.common.support.unit.TimeUnit;
 import com.chua.common.support.value.TimeValue;
@@ -14,8 +15,6 @@ import java.lang.reflect.Method;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 /**
  * @author CH
@@ -29,42 +28,77 @@ public class CacheMethodAnnotationPostProcessor implements MethodAnnotationPostP
     @SuppressWarnings("ALL")
     public Object execute(MethodDescribe describe, Object entity, Object[] args) {
         Cache cache = describe.getAnnotation(Cache.class);
-        String timeout = cache.timeout();
-        String type = cache.type();
-        Cacheable<String, Object> cacheable = referenceHashMap.computeIfAbsent(type, new Function<String, Cacheable<String, Object>>() {
-            @Override
-            public Cacheable<String, Object> apply(String s) {
-                Cacheable<String, Object> cacheable = ServiceProvider.of(Cacheable.class).getExtension(type);
-                cacheable.configuration(CacheConfiguration.builder().build());
-                return cacheable;
-            }
-        });
+        if(null != cache) {
+            return inCache(cache, describe, entity,  args);
+        }
 
-        return cacheable.getOrPut(getKey(entity, args), new Supplier<Object>() {
-            @Override
-            public Object get() {
-                Duration duration = Duration.ofSeconds(TimeUnit.parse(timeout).toSecond());
-                Object value = null;
-                try {
-                    if(describe.hasBean()) {
-                        value = describe.bean();
-                    } else {
-                        value = describe.execute(entity, args);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return TimeValue.of(value, duration);            }
-        }).getValue();
+        CacheEvict cacheEvict = describe.getAnnotation(CacheEvict.class);
+        return outCache(cacheEvict, describe, entity,  args);
     }
 
-    private String getKey(Object entity, Object[] args) {
-        return "" + entity.hashCode() + Arrays.hashCode(args);
+    /**
+     * 清除缓存
+     *
+     * @param cacheEvict 缓存驱逐
+     * @param describe   描述
+     * @param entity     实体
+     * @param args       args
+     * @return {@link Object}
+     */
+    private Object outCache(CacheEvict cacheEvict, MethodDescribe describe, Object entity, Object[] args) {
+        String type = cacheEvict.type();
+        Cacheable<String, Object> stringObjectCacheable = referenceHashMap.get(type);
+        if(null != stringObjectCacheable) {
+            stringObjectCacheable.remove(getKey(describe, args));
+        }
+        try {
+            return describe.execute(entity, args);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     *缓存
+     *
+     * @param cache    缓存
+     * @param describe 描述
+     * @param entity   实体
+     * @param args     args
+     * @return {@link Object}
+     */
+    @SuppressWarnings("ALL")
+    private Object inCache(Cache cache, MethodDescribe describe, Object entity, Object[] args) {
+        String timeout = cache.timeout();
+        String type = cache.type();
+        Cacheable<String, Object> cacheable = referenceHashMap.computeIfAbsent(type, s -> {
+            Cacheable<String, Object> cacheable1 = ServiceProvider.of(Cacheable.class).getExtension(type);
+            cacheable1.configuration(CacheConfiguration.builder().build());
+            return cacheable1;
+        });
+
+        return cacheable.getOrPut(getKey(describe, args), () -> {
+            Duration duration = Duration.ofSeconds(TimeUnit.parse(timeout).toSecond());
+            Object value = null;
+            try {
+                if(describe.hasBean()) {
+                    value = describe.bean();
+                } else {
+                    value = describe.execute(entity, args);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return TimeValue.of(value, duration);            }).getValue();
+    }
+
+    private String getKey(MethodDescribe describe, Object[] args) {
+        return describe.method().getDeclaringClass().getTypeName() + "#" + describe.method().getName() + Arrays.hashCode(args);
     }
 
     @Override
     public boolean hasAnnotation(Method method) {
-        return method.isAnnotationPresent(Cache.class);
+        return method.isAnnotationPresent(Cache.class) || method.isAnnotationPresent(CacheEvict.class);
     }
 
 
