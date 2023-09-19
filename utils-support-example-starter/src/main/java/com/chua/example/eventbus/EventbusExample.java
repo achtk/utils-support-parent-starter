5af26845ea776829b3c932c28a1e20f7f5b9f9c3
ACHTK <yemengzhidiandig@gmail.com>
2023-09-19 10:30:42 +0800

    fix 1.消息总线

--- utils-support-example-starter/src/main/java/com/chua/example/eventbus/EventbusExample.java
+++ utils-support-example-starter/src/main/java/com/chua/example/eventbus/EventbusExample.java
@@ -1,6 +1,5 @@
 package com.chua.example.eventbus;
 
-import com.chua.common.support.eventbus.BinlogSubscribeEventbus;
 import com.chua.common.support.eventbus.Eventbus;
 import com.chua.common.support.utils.ThreadUtils;
 
@@ -12,7 +11,7 @@ public class EventbusExample {
     public static void main(String[] args) throws Exception {
         Eventbus eventbus = Eventbus.newDefault();
 //        eventbus.registerSubscriber("zbus" , new ZbusSubscribeEventbus());
-        eventbus.registerSubscriber("binlog", new BinlogSubscribeEventbus("112.124.44.21:3306", "root", "rootDB@qrkj"));
+//        eventbus.registerSubscriber("binlog", new BinlogSubscribeEventbus("xxxxx:3306", "root", "root"));
 //        eventbus.registerObject(new SubscribeEntity());
 //        eventbus.registerObject(new SubscribeEntity1());
         eventbus.registerObject(new BinlogSubscribeEntity());
