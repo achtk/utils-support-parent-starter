e3311a1e429a7a27105435fbb3da23620f3c7cae
ACHTK <yemengzhidiandig@gmail.com>
2023-09-20 20:53:13 +0800

    fix 1.统一配置中心添加监控管理

--- utils-support-sshd-starter/src/main/java/com/chua/sshd/support/client/SshClient.java
+++ utils-support-sshd-starter/src/main/java/com/chua/sshd/support/client/SshClient.java
@@ -10,10 +10,7 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
@@ -32,8 +29,8 @@ public class SshClient extends AbstractClient<SshClient> {
 
     private final Map<String, Consumer<String>> consumerMap = new ConcurrentHashMap<>();
     private Session session;
-    private OutputStreamWriter writer;
     private BufferedReader reader;
+    private PrintWriter writer;
     private ChannelShell shell;
 
     public SshClient(ClientOption clientOption) {
@@ -63,7 +60,7 @@ public class SshClient extends AbstractClient<SshClient> {
             shell = (ChannelShell) session.openChannel("shell");
             shell.connect();
 
-            writer = new OutputStreamWriter(shell.getOutputStream(), Projects.defaultCharset());
+            writer = new PrintWriter(new OutputStreamWriter(shell.getOutputStream(), Projects.defaultCharset()));
             reader = new BufferedReader(new InputStreamReader(shell.getInputStream(), Projects.defaultCharset()));
             service.execute(() -> {
                 StringBuffer buffer = new StringBuffer();
@@ -71,7 +68,7 @@ public class SshClient extends AbstractClient<SshClient> {
                     String line = null;
                     while ((line = reader.readLine()) != null) {
                         for (Consumer<String> consumer : consumerMap.values()) {
-                            consumer.accept(line);
+                            consumer.accept(line + "\r\n");
                         }
                     }
                 } catch (IOException e) {
@@ -126,11 +123,8 @@ public class SshClient extends AbstractClient<SshClient> {
      * @param message 消息
      */
     public void send(String message) {
-        try {
-            writer.write(message);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        writer.write(message);
+        writer.flush();
     }
 
     /**
